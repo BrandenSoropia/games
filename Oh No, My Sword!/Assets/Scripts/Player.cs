c486e3baf2931d754142317670c6456db35276b3
Branden Soropia <b.soropia@gmail.com>
2018-09-02 15:11:07 -0400

    Movement, collision and breakable walls

---
+++ Oh No, My Sword!/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float movementSpeed = 10f;
+
+	private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		rb2D.MovePosition(new Vector2(rb2D.position.x + horizontal, rb2D.position.y + vertical));
+	}
+
+		void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Breakable")
+		{
+			Debug.Log("Removing" + collision.gameObject.name);
+			collision.gameObject.SetActive(false);
+		}
+    }
+}
---
+++ Oh No, My Sword!/Assets/Scripts/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	private GameObject gameObject;
+
+	void Start()
+	{
+		gameObject = GetComponent<GameObject>();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+}
