30d03cb8376ad933cdfba614de8bcf8ced1c3160
Branden Soropia <b.soropia@gmail.com>
2018-09-03 15:56:53 -0400

    Logic for health. Prevent player (and hopefully enemies from going through walls when moving too fast

--- Oh No, My Sword!/Assets/Scripts/Player.cs
+++ Oh No, My Sword!/Assets/Scripts/Player.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	public int health = 3;
 	public float movementSpeed = 10f;
 	public float decelerateSpeed = 0.9f;
+	public float invulnerabilityAfterDamageTakenTimeSeconds = 1f;
+	private float timeDamageTaken;
+	public bool isInvulnerable = false;
 	private Vector2 movement;
 
 	private Rigidbody2D rb2D;
@@ -31,13 +35,33 @@ public class Player : MonoBehaviour {
 			rb2D.velocity = rb2D.velocity * decelerateSpeed;
             rb2D.angularVelocity = rb2D.angularVelocity * decelerateSpeed; 
 		}
+	
+		if (isInvulnerable && (Time.time - timeDamageTaken > invulnerabilityAfterDamageTakenTimeSeconds))
+		{
+			Debug.Log("Not Invulnerable!");
+			isInvulnerable = false;
+		}
+	}
+
+	void UpdateHealth(int value)
+	{
+		health += value;
+		Debug.Log("Player Health: " + health);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
     {
+		float currentTime = Time.time;
         if (collision.gameObject.tag == "Breakable")
 		{
 			collision.gameObject.SetActive(false);
+		}
+		else if ((collision.gameObject.tag == "Enemy") && !isInvulnerable)
+		{
+			Debug.Log("Invulnerable!");
+			UpdateHealth(-1);
+			timeDamageTaken = currentTime;
+			isInvulnerable = true;
 		}
     }
 }
